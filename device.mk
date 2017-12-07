45e0005ff2c386ac47fe5ed83e9c600ddfb731dd
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 22:15:14 +0800

    kenzo: Build a dummy android.hidl.base@1.0 for Oreo blobs
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c
    Signed-off-by: TheDarkStrix <thedarkstrix@gmail.com>

--- device.mk
+++ device.mk
@@ -74,6 +74,8 @@ PRODUCT_PACKAGES += \
     AntHalService \
     com.dsi.ant.antradio_library \
     libantradio
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0
 
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
