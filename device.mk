53379f4cc8f4cb8e227cb6802d0da2b58a6410b4
thedarkstrix <thedarkstrix@gmail.com>
2018-01-18 14:35:13 +0000

    kenzo: hidl: Use QTI power HAL

--- device.mk
+++ device.mk
@@ -312,8 +312,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    power.msm8952
+    android.hardware.power@1.0-service-qti
 
 # Protobuf
 PRODUCT_PACKAGES += \
