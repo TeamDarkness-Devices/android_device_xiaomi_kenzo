c87e7cb0ba2caea48944d07a86fd3b7936e121c2
thedarkstrix <thedarkstrix@gmail.com>
2018-01-28 07:25:53 +0000

    kenzo: aos: remove lineage specific overlays
    * Already defined  and this is useless for aos.
    
    Signed-off-by: thedarkstrix <thedarkstrix@gmail.com>
    Signed-off-by: TheDarkStrix <thedarkstrix@gmail.com>

--- device.mk
+++ device.mk
@@ -22,8 +22,7 @@ $(call inherit-product-if-exists, vendor/xiaomi/kenzo/kenzo-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
